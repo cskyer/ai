81995fb21dd4e3c9b70da8add7cbc53822b6fb56
qq2575896094 <2575896094@qq.com>
2024-04-26 16:41:24 +0800

    fix(logo): fix header logo

--- app/components/header-logo/index.tsx
+++ app/components/header-logo/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const HeaderLogo = () => {
     return (
         <Flex className="rt-r-pl-4 rt-r-ai-center rt-r-overflow-hidden flex-shrink-0" width="250px" height="inherit">
-            <Image src="/logo.svg" width={60} height={48} alt="AIGC"/>
+            <Image src="/logo.svg" priority width="60" height={19} alt="AIGC"/>
         </Flex>
     )
 }
